739ecb1af844ebc8850ca32a281f79df18795e07
Ax2004-2003 <8232200102@univermilenium.edu.mx>
2025-08-11 09:50:21 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,42 @@
+const CACHE_NAME = 'ciberseguridad-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/chat1.jpg',
+    '/chat2.jpg',
+    'https://cdn.tailwindcss.com',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap'
+];
+
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('mi-pwa-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        'dark.html',
-        'manifest.json',
-        'chat1.jpg',
-        'chat2.jpg'
-      ]);
-    })
-  );
-  console.log('Service Worker instalado');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
